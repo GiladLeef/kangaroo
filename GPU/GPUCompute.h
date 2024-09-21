6fd39f67cfdd4bad0e365ae822809853f0fc90cb
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-09-21 17:02:32 +0300

    Update GPUCompute.h

--- GPU/GPUCompute.h
+++ GPU/GPUCompute.h
@@ -18,39 +18,30 @@ __device__ void ComputeKangaroos(uint64_t *kangaroos,uint32_t maxFound,uint32_t 
 
     // P1 = jumpPoint
     // P2 = kangaroo
-
+    
     __syncthreads();
-
+    
     for(int g = 0; g < GPU_GRP_SIZE; g++) {
       jmp = (uint32_t)px[g][0] & (NB_JUMP-1);
-
       ModSub256(dx[g],px[g],jPx[jmp]);
     }
-
     _ModInvGrouped(dx);
-
     __syncthreads();
 
     for(int g = 0; g < GPU_GRP_SIZE; g++) {
-
       jmp = (uint32_t)px[g][0] & (NB_JUMP-1);
-
       ModSub256(dy,py[g],jPy[jmp]);
       _ModMult(_s,dy,dx[g]);
       _ModSqr(_p,_s);
-
       ModSub256(rx,_p,jPx[jmp]);
       ModSub256(rx,px[g]);
-
       ModSub256(ry,px[g],rx);
       _ModMult(ry,_s);
       ModSub256(ry,py[g]);
-
       Load256(px[g],rx);
       Load256(py[g],ry);
-
       Add128(dist[g],jD[jmp]);
-
+      
       if((px[g][3] & dpMask) == 0) {
 
         // Distinguished point
@@ -59,11 +50,8 @@ __device__ void ComputeKangaroos(uint64_t *kangaroos,uint32_t maxFound,uint32_t 
           uint64_t kIdx = (uint64_t)IDX + (uint64_t)g*(uint64_t)blockDim.x + (uint64_t)blockIdx.x*((uint64_t)blockDim.x*GPU_GRP_SIZE);
           OutputDP(px[g],dist[g],&kIdx);
         }
-
       }
-
     }
-
   }
   StoreKangaroos(kangaroos,px,py,dist);
 }
